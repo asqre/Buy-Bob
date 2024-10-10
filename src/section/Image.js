892bb0b17041a71fca9bc4c0d4168e66c8a25f8c
asqre <amitanand.asqre@gmail.com>
2024-10-10 17:34:59 +0530

    Refactor font styles and color in index.css, and update image scaling in Image.js

--- src/section/Image.js
+++ src/section/Image.js
@@ -2,28 +2,30 @@ import React, { useEffect, useState } from "react";
 import { FaTelegramPlane } from "react-icons/fa";
 
 function Image() {
-  const [scale, setScale] = useState(0.6); 
+  const [scale, setScale] = useState(0.6);
 
   useEffect(() => {
-   
     const timer = setTimeout(() => {
       setScale(1);
-    }, 100); 
+    }, 100);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className="relative flex flex-col items-center h-[150vh] bg-[#46499e] pt-[110px] pb-[100px]">
-      <div className="text-white text-[150px] z-10 flex-grow font-semibold">bob the blob</div>
+    <div className="relative flex flex-col justify-center items-center h-[70vh] sm:h-[80vh] md:h-[100vh] lg:h-[110vh] bg-[#46499e] pt-[110px] pb-[100px]">
+      <h1 className="absolute top-[10vh] z-10">bob the blob</h1>
       <img
         src="https://buybob.xyz/images/cc19db0bbff0a2d701d437a40155c802.png"
         alt="bob"
-        className={`absolute top-[40px] w-1/2 h-auto z-0 transform transition-transform duration-1000 ease-in-out`}
-        style={{ transform: `scale(${scale})`, opacity: scale }} 
+        className={`absolute bottom-[20vh] lg:w-1/2 h-auto z-0 transform transition-transform duration-1000 ease-in-out`}
+        style={{ transform: `scale(${scale})`, opacity: scale }}
       />
-      <div className="flex justify-center items-center z-10 text-white">
-        <div className="text-[50px] w-[200px] leading-none">Join the telegram</div>
+      <div className="absolute bottom-[10vh] flex z-10 text-white gap-5">
+        <h3 className="leading-none">
+          Join the <br />
+          telegram
+        </h3>
         <FaTelegramPlane size={100} />
       </div>
     </div>
