ebba2637bcc572f68f4c330412299df79b733cc6
Shantanu-2127 <Shantanusisodia777@gmail.com>
2024-10-15 12:08:30 +0530

    border

--- src/components/FlexBoxComp.js
+++ src/components/FlexBoxComp.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function FlexBoxComp({ images }) {
   return (
-    <div className="w-full sm:w-[80%] md:w-1/3 flex flex-col gap-8 mx-auto">
+    <div className="w-full sm:w-[80%] md:w-1/3 flex flex-col gap-5 mx-auto">
       {images.map((image, index) => (
         <img
           key={index}
           src={image.src}
           alt="bob"
-          className="w-full h-auto object-contain"
+          className="w-full h-auto object-contain border-2 border-black"
         />
       ))}
     </div>
