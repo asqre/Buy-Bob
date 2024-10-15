2cfa8f2522f895112ed3a76d3eb853f1ab6a8119
asqre <amitanand.asqre@gmail.com>
2024-10-15 11:48:39 +0530

    fixed css

--- src/section/Buy.js
+++ src/section/Buy.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { companyImages } from "../data";
 import CustomSVGPath from "../components/CustomPath";
+import dexscreener from "../assets/images/dexscreener.png";
+import { FaClipboard, FaClipboardCheck } from "react-icons/fa";
 
 const Buy = () => {
+  const [copied, setCopied] = useState(false);
+  const contractAddress =
+    "0x5f3a18cdfd7ef0527a65ba5c07dbe0efe276507d4d1a4d1bebe87f5d40df6cf6::bob::BOB";
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(contractAddress);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
     <div className="relative flex flex-col gap-[32px] w-full mx-auto my-[80px] md:my-[150px] xl:mt-[400px] 2xl:mt-[550px] px-[10%] sm:px-[15%] md:px-[10%] lg:px-[15%] bg-gradient-to-r from-[#A1C8E8] to-[#E0F7FA]">
       <h2>How to buy cwk</h2>
@@ -12,10 +24,14 @@ const Buy = () => {
         visit any SUI based DEX and trade ​your $SUI for $BOB.
       </h5>
 
-      <h4 className="break-words">
-        CA:
-        0x5f3a18cdfd7ef0527a65ba5c07dbe0efe276507d4d1a4d1bebe87f5d40df6cf6::bob::BOB
-      </h4>
+      <div className="flex gap-4 items-center">
+        <h4 className="break-words">CA: {contractAddress}</h4>
+        <button
+          onClick={copyToClipboard}
+        >
+          {copied ? <FaClipboardCheck  size={30}/> : <FaClipboard size={30} />}
+        </button>
+      </div>
 
       <div className="flex flex-wrap justify-center gap-20 py-10">
         {companyImages.map((company) => (
@@ -29,9 +45,23 @@ const Buy = () => {
       </div>
 
       <div className="flex flex-col md:flex-row items-center sm:items-start justify-between gap-8">
-        <div className="flex flex-col gap-5 w-full md:w-[30%] items-center md:items-start">
-          <CustomSVGPath img="https://buybob.xyz/images/dce615e04e1298ade3fa1ca428b3100e.png" name={"BIRDEYE"} bgColor={"#1965FD"}/>
-          <CustomSVGPath />
+        <div className="flex flex-col gap-8 w-full md:w-[30%] items-center md:items-start">
+          <CustomSVGPath
+            img="https://buybob.xyz/images/dce615e04e1298ade3fa1ca428b3100e.png"
+            name={"BIRDEYE"}
+            bgColor={"#1965FD"}
+            link={
+              "https://birdeye.so/token/0x5cc33d0aabadf21d977b6249b417750ce34b52a7a144e61bf9874413214d2364::cwk::CWK?chain=sui"
+            }
+          />
+          <CustomSVGPath
+            img={dexscreener}
+            name={"DEX SCREENER"}
+            link={
+              "https://dexscreener.com/sui/0x199bb56fb7f9934b92585721d10b267534d4f3c667db4f26a44437d4d8f9da1b"
+            }
+          />
+          {/* <CustomSVGPath />
           <h4>
             Built by{" "}
             <span className="underline">
@@ -43,22 +73,23 @@ const Buy = () => {
                 Memespace
               </a>
             </span>
-          </h4>
+          </h4> */}
         </div>
 
         <div className="flex flex-col gap-8 w-full items-center md:items-start md:w-[60%]">
-          <CustomSVGPath img="https://buybob.xyz/images/36220bc05d390737f6fd6885fb2dc7f5.svg" name={"DEX TOOLS"}/>
+          {/* <CustomSVGPath
+            img="https://buybob.xyz/images/36220bc05d390737f6fd6885fb2dc7f5.svg"
+            name={"DEX TOOLS"}
+          /> */}
 
-          <h6 className="text-xs sm:text-sm lg:text-base text-center sm:text-left">
-            $BOB is a meme token and should not be considered as an investment
-            vehicle or a medium ​for profit. The value of $BOB is not promised
-            or guaranteed. The primary purpose of $BOB is ​to encourage
-            ​community engagement and facilitate participation in a
-            community-driven meme ​culture project. Purchasing $BOB should be
-            regarded as a form of participation and support ​for the project,
-            not as a ​financial investment with an expectation of profit. By
-            engaging with ​this project, you acknowledge that you understand and
-            accept these terms.
+          <h6 className="text-xs sm:text-sm lg:text-2xl text-center sm:text-left">
+            Crab with Knife: You’ve heard of bulls & bears. Forget them. The
+            real market animal is a crab—armed, armored, and ready to slice up
+            the competition. Grab your knife! This coin doesn’t just have
+            utility—it has a knife. Why trust boring old charts when you can
+            have a crab wielding a knife? Join the sharpest community in crypto!
+            Armed with humor and low fees, Crab with Knife is here to take over
+            the sui blockchain!
           </h6>
         </div>
       </div>
--- src/section/Image.js
+++ src/section/Image.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaTelegramPlane } from "react-icons/fa";
 import immigration from "../assets/images/img2.png";
 import { FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 function Image() {
   const [scale, setScale] = useState(0.6);
@@ -16,14 +17,11 @@ function Image() {
 
   return (
     <div className="relative flex flex-col justify-center items-center  xl:h-[65vh]">
-      <h1 className="absolute top-[15vh] z-10">
-        Crab with knife
-      </h1>
+      <h1 className="absolute top-[15vh] z-10">Crab with knife</h1>
       <img
         src={immigration}
         alt="bob"
         className="h-[80vh] w-[100vw] object-cover lg:w-auto lg:h-auto"
-       
       />
       <div
         className=" flex flex-row justify-around text-[#24A1DE] gap-5 py-8  w-[100vw]"
@@ -34,13 +32,17 @@ function Image() {
             Join the <br />
             telegram
           </h1>
-          <FaTelegramPlane className="w-[40px] h-[40px]" />
+          <a href="https://t.me/+5WcQbex5ixAyM2Q5" target="_blank">
+            <FaTelegramPlane className="w-[40px] h-[40px] md:w-16 md:h-16 lg:w-24 lg:h-24" />
+          </a>
         </div>
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-white text-2xl md:text-5xl font-bold leading-none">
             Join with <br /> twitter
           </h1>
-          <FaXTwitter className="w-[40px] h-[40px]"  color="black" />
+          <a href="https://x.com/crabwithknifex" target="_blank">
+            <FaXTwitter className="w-[40px] h-[40px] md:w-16 md:h-16 lg:w-24 lg:h-24" color="black" />
+          </a>
         </div>
       </div>
     </div>
